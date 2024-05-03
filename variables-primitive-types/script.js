25852fa53029185d5af91bd620801b5dcfcc8740
Egide Hirwa <66557478+egidehirwa@users.noreply.github.com>
2024-05-03 18:58:23 +0200

    Exercices on variables and primitive types

---
+++ variables-primitive-types/script.js
@@ -0,0 +1,41 @@
+/* Const cannot be reassigned (let can be reassigned)
+
+
+let name = "Egide";
+console.log(name);
+
+name = "Jean Jacques Goldman"
+console.log(name);
+
+name = true;
+console.log(name);
+
+name = 42;
+console.log(name); 
+*/
+
+
+/* 
+// const needs to be initialized while being declared.
+// Once declared a const variable is frozen.
+
+let value;
+value = "Moo!";
+console.log(value);
+
+const value2;
+value2 = "Molo!";
+console.log(value2);
+ */
+
+// Using the typeof operator.
+
+const name = "Egide";
+let age = 30;
+let student = true;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof student);
+
+console.log(typeof name, typeof age, typeof student);
