d923193b5989d38a5c0430b7e051dd74e52033a7
egidehirwa <egidehirwam@gmail.com>
2024-12-10 22:54:40 +0100

    math operations

---
+++ math-operations/assignments-math.js
@@ -0,0 +1,53 @@
+//  Mathematical assighnements - shortcuts to shorten the amount of code written
+
+// ++ increment by 1
+
+let points = 10;
+
+points++
+
+console.log(points);
+
+// -- decrement by 1
+
+let score = 20;
+
+score--
+
+console.log(score);
+
+// += increment by value
+
+score += 20;
+console.log(score);
+
+// -= decrement by value
+
+score -= 19;
+
+console.log(score);
+
+// *= multiply by value
+
+let age = 7;
+
+age *= 2
+
+console.log(age);
+
+// /= divide by value
+
+age /= 2;
+
+console.log(age);
+
+// %= modulus by value
+
+// age %= 7;
+
+console.log(age);
+
+// **= exponentiate by value
+
+age **= 2;
+console.log(age);
---
+++ math-operations/script.js
@@ -0,0 +1,37 @@
+// + addition, - subtraction, * multiplication, / division, % modulus (remainder), ** exponent
+
+// Addition
+
+let age = 10 + 23;
+
+console.log(age);
+
+// Subtraction
+
+let minus = 23 - 50;
+
+console.log(minus);
+
+// Multiplication
+
+let result = 20 * 45;
+
+console.log(result);
+
+// Division
+
+let rest = 45 / 56;
+
+console.log(rest);
+
+// Modulus - remainder
+
+let mod = 7 % 3;
+
+console.log(mod);
+
+// Exponentiation
+
+let exp = 4 ** 2;
+
+console.log(exp);
