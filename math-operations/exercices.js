e28165f12a9f085dc128264aba7230f4b878a16c
egidehirwa <egidehirwam@gmail.com>
2024-12-11 09:53:27 +0100

    math operation exercices

---
+++ math-operations/exercices.js
@@ -0,0 +1,62 @@
+// Math operation exercices
+
+/* 
+// Exercice 1
+
+let name = "Egide";
+
+name += " Hirwa-Mugabe";
+
+console.log(name);
+
+// Exercice 2
+
+let chiffre = 25;
+
+chiffre += 34;
+
+console.log(chiffre);
+ */
+
+/* 
+// Problem set 1
+
+console.log(5 / 2);
+
+console.log(5 % 2);
+
+console.log(5 ** 2);
+ */
+
+/* 
+// Problem set 2
+
+let points
+points = 20;
+points += 15;
+console.log(points);
+
+// Using const will not work because we'll be reassigning the variable, which is not allowed
+ */
+
+/* 
+// Problem set 3
+
+console.log(5 / 5);
+console.log(5 % 5);
+console.log(256 / 2);
+console.log(256 % 10);
+
+// All possible values of ANY_NUMBER % 2 are (0, 1)
+// All possible values of   ANY_NUMBER % 10 are (0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+ */
+
+/* 
+// BONUS
+
+let bools = true + true + true // 3
+
+// js interprets true to equal 1, so the addition above is treated as 1 + 1 + 1
+
+console.log(bools);
+ */
