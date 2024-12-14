bacb28b4070b0e6bdc1f21485e675d3f3d7f397b
egidehirwa <egidehirwam@gmail.com>
2024-12-14 23:58:00 +0100

    boolean operators problem sets

---
+++ boolean-operations/problem-set-1.js
@@ -0,0 +1,33 @@
+//  Writing out the truth tales for AND, OR, NOT
+
+// AND truth table
+
+console.log("-----AND-----");
+console.log(true && true); // true
+console.log(true && false); // false
+console.log(false && true); // false
+console.log(false && false); // false
+
+// OR truth table
+
+console.log("-----OR-----");
+console.log(true || true); // true
+console.log(true || false); // true
+console.log(false || true); // true
+console.log(false || false); // false
+
+// NOT truth table
+
+console.log("-----NOT-----");
+console.log(!true); // false
+console.log(!false); // true
+
+
+// Problem set
+
+console.log("-----PROBLEM SET------")
+
+console.log("1" === 1); // false
+console.log((3>2) === true); // true
+console.log((2**2) !== "4"); // true
+console.log((true === true) && (false === false)); // true
